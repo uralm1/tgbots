ece9e4ae2855b6cf3abd9d474793d11191d53194
uralm1 <uralm1@gmail.com>
2023-12-31 23:48:23 +0500

    Reorganize lib paths

--- pibot/main.cpp
+++ pibot/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char* argv[]) {
   if (argc >= 2 &&
       (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0 ||
        argc > 2)) {
-    cout << "Help: sbot [telegram.yml]" << endl;
+    cout << "Help: sbot [pibot.yml]" << endl;
     exit(EXIT_SUCCESS);
   }
 
@@ -17,7 +17,7 @@ int main(int argc, char* argv[]) {
   if (argc == 2) {
     config_file = argv[1];
   } else {
-    config_file = "telegram.yml";
+    config_file = "pibot.yml";
   }
 
   static PiBotApp app(config_file);
